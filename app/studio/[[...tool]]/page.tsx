c1d79b588145dd6ce8a45d39b33470d63c0c4252
Reaper500 <zinakibang@gmail.com>
2026-06-29 09:47:24 +0100

    fix: resolve static file config

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -3,6 +3,15 @@
 import { NextStudio } from "next-sanity/studio";
 import config from "@/sanity.config";
 
+export const dynamic = "force-static";
+
+export function generateStaticParams() {
+  return [
+    { tool: [] }
+  ];
+}
+
+
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
